464152d5ceaa5fc66fdc3bbf773a9052547444ce
warlock-PC <vlasovmax1706@icloud.com>
2024-12-15 12:41:05 +0200

    work with templates

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Setup", "First App", "Contacts"]
+
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html', title="Main Page", menu=menu)
+
+@app.route('/about')
+def about():
+    return render_template('index.html', title="About Website", menu=menu)
 
 if __name__ == '__main__':
     app.run(debug=True)
